ae6c270711bd864df40411a786358ef94372e725
sai sankar bhuvanapalli <40035693+saisankar20@users.noreply.github.com>
2023-12-05 23:17:51 -0500

    Update noderanking.cpp

--- noderanking.cpp
+++ noderanking.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <algorithm>
-
-using namespace std;
+#include <climits>
 
 class Graph {
 public:
     int vertices;
-    vector<vector<int> > adjacencyList;
+    std::vector<std::vector<int>> adjacencyList;
 
     Graph(int v) : vertices(v), adjacencyList(v) {}
 
@@ -17,32 +15,31 @@ public:
         adjacencyList[v].push_back(u);
     }
 
-    vector<int> getNodeRanking(int startNode) {
-        vector<int> nodeRanking(vertices, -1); 
-        queue<int> q;
+    std::vector<int> calculateNodeRanks(int startNode) {
+        std::vector<int> nodeRanks(vertices, INT_MAX);
+        std::queue<int> q;
         q.push(startNode);
-        nodeRanking[startNode] = 3; 
+        nodeRanks[startNode] = 0;
 
         while (!q.empty()) {
             int current = q.front();
             q.pop();
 
-            for (vector<int>::iterator it = adjacencyList[current].begin(); it != adjacencyList[current].end(); ++it) {
-                int neighbor = *it;
-                if (nodeRanking[neighbor] == -1) {
-                    nodeRanking[neighbor] = nodeRanking[current] + 1;
+            for (int neighbor : adjacencyList[current]) {
+                if (nodeRanks[neighbor] == INT_MAX) {
+                    nodeRanks[neighbor] = nodeRanks[current] + 1;
                     q.push(neighbor);
                 }
             }
         }
 
-        return nodeRanking;
+        return nodeRanks;
     }
 };
 
 int main() {
-    int vertices = 10;
-    Graph graph(vertices);
+    const int numVertices = 10;
+    Graph graph(numVertices);
 
     graph.addEdge(4, 1);
     graph.addEdge(4, 7);
@@ -51,7 +48,6 @@ int main() {
     graph.addEdge(1, 5);
     graph.addEdge(1, 2);
     graph.addEdge(1, 3);
-    graph.addEdge(2, 4);
     graph.addEdge(2, 3);
     graph.addEdge(2, 5);
     graph.addEdge(7, 8);
@@ -60,13 +56,12 @@ int main() {
     graph.addEdge(9, 6);
     graph.addEdge(8, 6);
 
-    int startNode = 1;
-    vector<int> nodeRanking = graph.getNodeRanking(startNode);
+    const int startNode = 1;
+    std::vector<int> nodeRanks = graph.calculateNodeRanks(startNode);
 
-    
-    cout << "Node Ranking:\n";
-    for (int i = 0; i < vertices; ++i) {
-        cout << "Node " << i << ": " << nodeRanking[i] << "\n";
+    std::cout << "Node Ranks:\n";
+    for (int i = 0; i < numVertices; ++i) {
+        std::cout << "Node " << i << ": " << nodeRanks[i] << "\n";
     }
 
     return 0;
